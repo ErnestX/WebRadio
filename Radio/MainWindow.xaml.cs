0cf772ca5625180e385092d3bf0ed8c778f8b17e
ErnestX <xiangspecial2012@gmail.com>
2020-07-08 10:36:16 -0400

    First commit

---
+++ Radio/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using NLog;
+using NLog.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Radio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        public MainWindow()
+        {   
+            //Debug.WriteLine("################## Initalizing Main Window ###################");
+            Logger.Debug("---------test---------");
+            InitializeComponent();
+            //Debug.WriteLine("################## Initalized Main Window ####################");
+            
+        }
+    }
+}
---
+++ Radio/RadioViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using NLog.Fluent;
+
+namespace Radio
+{
+    class RadioViewModel : INotifyPropertyChanged
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //constructor
+        public RadioViewModel()
+        {
+        }
+
+        public string PlayCommand()
+        {
+            Logger.Debug("---------test---------");
+            return null;
+        }
+    }
+}
