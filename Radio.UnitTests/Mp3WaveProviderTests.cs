11c467b79db729608a861c4d38ae8e23d36977f3
ErnestX <xiangspecial2012@gmail.com>
2020-09-14 15:53:34 -0400

    refactored tests and implemented new reader test in the case the increment is larger than buffer size

--- Radio.UnitTests/Mp3WaveProviderTests.cs
+++ Radio.UnitTests/Mp3WaveProviderTests.cs
@@ -26,72 +26,71 @@ namespace Radio.UnitTests
         }
 
         [Test]
-        public void CreateNewProviderFromSoundcloud_ValidAndSuccessful_NoExceptions() {
-            // TODO: move to SoundCloudResourceFinderTest and use a SoundCloud url
-            Uri uri = new Uri("https://cf-media.sndcdn.com/w9nHQlnktPK7.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vdzluSFFsbmt0UEs3LjEyOC5tcDMiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE1OTg0MTI1NTh9fX1dfQ__&Signature=MlDwwgfG-Myv8udL0A~5lHMc7e7Yf-OGzIir6YpBXkMdrfiXiux0TbdWkkG6bkSBXFzwO6oZE-PhLeAejW7drTRgvJtA5wxCxRjSfGRZ2OTsYN5RwK7OrxiKRYeWz8aYK8exFy2fYAEdvAJvpykupxLAV-Jen3I3kpVIciZCwqyVPeAIlVOBMmp9ERvRgFEE8XI5n0Da00l8spQuhjT7XstWYaFXEGraBUNkoiNxbCASRelneV5MOw3IURTctoe9x3-xx5KaOhPPnWLSnjNihivG0m56u2bQ6VkcM1H3IvzQJMpSslQby4ucn~G3jUqq~0vmPKB6~jmIz0CjuUGoFQ__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ");
-            Assert.DoesNotThrow(() => new Mp3WaveProvider(uri, 20480));
+        public void Read_BufferSizeEqualToReadIncrement_OutputCorrectly()
+        {
+            ReadTestHelper(VALID_MP3_STREAM_1, 1024, 1024);
+            ReadTestHelper(VALID_MP3_STREAM_1, 4096, 4096);
+            ReadTestHelper(VALID_MP3_STREAM_1, 4095, 4095);
         }
 
         [Test]
-        public void Read_WhenCalled_OutputCorrectly()
+        public void Read_BufferSizeLargerThanReadIncrement_OutputCorrectly()
         {
-            test(VALID_MP3_STREAM_1, 1024, 1024);
-            //test(VALID_MP3_STREAM_1, 4096, 1024); //TODO this fails: array not long enough
+            ReadTestHelper(VALID_MP3_STREAM_1, 2048, 1024);
+            ReadTestHelper(VALID_MP3_STREAM_1, 2047, 1024);
+            ReadTestHelper(VALID_MP3_STREAM_1, 4096, 1024); 
+        }
 
-            void test(string url, int bufferSize, int readIncrement)
+        public void ReadTestHelper(string url, int bufferSize, int readIncrement)
+        {
+            HttpWebRequest req;
+            HttpWebResponse tempRes = null;
+            Mp3WaveProvider mwp = null;
+
+            try
             {
-                HttpWebRequest req;
-                HttpWebResponse tempRes = null;
-                Mp3WaveProvider mwp = null;
+                req = (HttpWebRequest)WebRequest.Create(url);
+                tempRes = (HttpWebResponse)req.GetResponse();
+                Stream tempStream = tempRes.GetResponseStream();
 
-                try
+                byte[] expectedBuffer = new byte[1024 * 800];
+                int offset = 0;
+                int bytesRead;
+                do
                 {
-                    req = (HttpWebRequest)WebRequest.Create(url);
-                    tempRes = (HttpWebResponse)req.GetResponse();
-                    Stream tempStream = tempRes.GetResponseStream();
-
-                    byte[] expectedBuffer = new byte[1024 * 800];
-                    int offset = 0;
-                    int bytesRead;
-                    do
-                    {
-                        bytesRead = tempStream.Read(expectedBuffer, offset, readIncrement);
-                        offset += bytesRead;
-                    } while (bytesRead > 0);
+                    bytesRead = tempStream.Read(expectedBuffer, offset, readIncrement);
+                    offset += bytesRead;
+                } while (bytesRead > 0);
 
 
-                    mwp = new Mp3WaveProvider(new Uri(url), bufferSize);
-                    byte[] testBuffer = new byte[1024 * 800];
-                    offset = 0;
-                    do
-                    {
-                        bytesRead = mwp.Read(testBuffer, offset, readIncrement);
-                        offset += bytesRead;
-                    } while (bytesRead > 0);
+                mwp = new Mp3WaveProvider(new Uri(url), bufferSize);
+                byte[] testBuffer = new byte[1024 * 800];
+                offset = 0;
+                do
+                {
+                    bytesRead = mwp.Read(testBuffer, offset, readIncrement);
+                    offset += bytesRead;
+                } while (bytesRead > 0);
 
-                    Assert.IsTrue(testBuffer.Length == expectedBuffer.Length);
-                    for (int i = 0; i < expectedBuffer.Length; i++)
-                    {
-                        if (testBuffer[i] != expectedBuffer[i])
-                        {
-                            TestContext.Out.WriteLine("inequity found at index: {0}", i);
-                            Assert.Fail();
-                        }
-                    }
+                Assert.IsTrue(testBuffer.Length == expectedBuffer.Length);
+                for (int i = 0; i < expectedBuffer.Length; i++)
+                {       
+                    Assert.AreEqual(expectedBuffer[i], testBuffer[i],String.Format("inequity found at index: {0}", i));
                 }
-                finally
+            }
+            finally
+            {
+                if (tempRes != null)
                 {
-                    if (tempRes != null)
-                    {
-                        tempRes.Close();
-                    }
+                    tempRes.Close();
+                }
 
-                    if (mwp != null)
-                    {
-                        mwp.Dispose();
-                    }
+                if (mwp != null)
+                {
+                    mwp.Dispose();
                 }
             }
         }
     }
 }
+
